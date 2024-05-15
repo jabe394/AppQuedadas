94416bb36f4d09c2a5e5d364284014accb8702bf
jabe394 <151621516+jabe394@users.noreply.github.com>
2024-05-15 10:25:22 +0200

    Delete AppQuedadas directory

--- AppQuedadas/src/main/java/module-info.java
+++
@@ -1,7 +0,0 @@
-module com.mycompany.appquedadas {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires  java.sql; 
-    opens com.mycompany.appquedadas to javafx.fxml;
-    exports com.mycompany.appquedadas;
-}
